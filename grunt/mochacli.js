7cef1556d0772f6697a74fd7b4e4d276e89e2749
treep78 <treep78@gmail.com>
2016-12-13 13:24:02 -0500

    Initial Commit
    *Add everything from the express api template

---
+++ grunt/mochacli.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const testDatabase = 'mongodb://localhost/express-multer-api-test';
+
+module.exports = {
+  options: {
+    env: {
+      MONGODB_URI: testDatabase,
+      NODE_PATH: process.env.PWD,
+    },
+  },
+  all: ['<%= paths.src.spec %>'],
+};
