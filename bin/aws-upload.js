73227c022e18e419917d5ebe120f875bd3ca655f
treep78 <treep78@gmail.com>
2016-12-14 10:08:30 -0500

    add package mime stores upload in folder by data add extensions to urls

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -6,23 +6,31 @@ const fs = require('fs');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
+const mime = require('mime');
+const path = require('path');
+
 
 let file ={
   path: process.argv[2],
   title: process.argv[3]
 };
 
-console.log("File is: ",file);
+let contentType = mime.lookup(file.path);
+let ext = path.extname(file.path);
+
+let folder = new Date().toISOString().split('T')[0];;
+
+console.log("Folder is: ",folder);
 
 let stream = fs.createReadStream(file.path);
 let bucket = process.env.AWS_S3_BUCKET_NAME;
-console.log("Bucket is: ",bucket);
-let key = file.title;
+let key = `${folder}/${file.title}${ext}`;
 let params = {
   ACL: 'public-read',
   Bucket: bucket,
   Key: key,
-  Body: stream
+  Body: stream,
+  ContentType:  contentType
 };
 
 s3.upload(params, (err, data)=>{
