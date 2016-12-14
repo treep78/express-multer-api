9cb62552a4c3fb99fa393d251298c24fc5a864a1
treep78 <treep78@gmail.com>
2016-12-14 10:48:02 -0500

    refactor code for s3upload
    *refector cade and moved in the aws s3.upload functions to lib/ for use in API in the future.
    *Required the file from our comman line script so that we can test it from the command line and see we didn't break anything.
    s3 upload machanics acan now be used  if we requore hat file from anywhere in our api,and jjst needs to be passed a file.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -1,42 +1,11 @@
 'use strict';
 
 require('dotenv').load();
-const fs = require('fs');
-
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-
-const mime = require('mime');
-const path = require('path');
-
+const s3Upload = require('../lib/aws-s3-upload.js');
 
 let file ={
   path: process.argv[2],
   title: process.argv[3]
 };
 
-let contentType = mime.lookup(file.path);
-let ext = path.extname(file.path);
-
-let folder = new Date().toISOString().split('T')[0];;
-
-console.log("Folder is: ",folder);
-
-let stream = fs.createReadStream(file.path);
-let bucket = process.env.AWS_S3_BUCKET_NAME;
-let key = `${folder}/${file.title}${ext}`;
-let params = {
-  ACL: 'public-read',
-  Bucket: bucket,
-  Key: key,
-  Body: stream,
-  ContentType:  contentType
-};
-
-s3.upload(params, (err, data)=>{
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
+s3Upload(file);
